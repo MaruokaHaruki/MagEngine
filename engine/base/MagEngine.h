784580b87437e9ab26e87efe3bab714b0a10f259
MaruokaHaruki <146906545+MaruokaHaruki@users.noreply.github.com>
2025-02-28 10:27:13 +0900

    MagEngine基盤クラスの追加
    MagEngine.vcxprojファイルに`MagEngine.cpp`と`MagEngine.h`を追加しました。
    
    `MagEngine.cpp`ファイルを新規作成し、エンジン基盤クラスの基本構造を追加しました。
    
    `MagEngine.h`ファイルを新規作成し、エンジン基盤クラスの基本構造と必要なヘッダーファイルをインクルードしました。

---
+++ engine/base/MagEngine.h
@@ -0,0 +1,35 @@
+/*********************************************************************
+ * \file   MagEngine.h
+ * \brief  エンジン基盤クラス
+ * 
+ * \author Harukichimaru
+ * \date   February 2025
+ * \note   
+ *********************************************************************/
+#pragma once
+//========================================
+// Framework
+#include "WinApp.h"
+#include "DirectXCore.h"
+#include "ImguiSetup.h"
+#include "SrvSetup.h"
+#include "Input.h"
+#include "MAudioG.h"
+#include "Camera.h"
+// 共通部
+#include "SpriteSetup.h"
+#include "TextureManager.h"
+#include "ParticleSetup.h"
+#include "Object3dSetup.h"
+#include "ModelManager.h"
+#include "LineManager.h"
+// シーン
+#include "CameraManager.h"
+#include "SceneManager.h"
+#include "SceneFactory.h"
+
+///=============================================================================
+///						エンジン基盤クラス
+class MagEngine {
+};
+
