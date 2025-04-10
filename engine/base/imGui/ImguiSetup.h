2f1f57fa6728ef1dd5cf6dae561e172471a8eab5
MaruokaHaruki <146906545+MaruokaHaruki@users.noreply.github.com>
2025-04-11 01:08:53 +0900

    ImGui の統合と関連ファイルの追加
    `MagEngine.vcxproj` において、`externals\imgui` ディレクトリ内の ImGui 関連のソースファイルとヘッダーファイルが追加されました。これにより、プロジェクトに ImGui の機能が統合されます。
    
    `MagEngine.vcxproj.filters` において、`externals\imgui` ディレクトリ内の ImGui 関連のソースファイルとヘッダーファイルがフィルタに追加されました。
    
    `ImguiSetup.h` において、`imgui.h` のインクルード位置が変更されました。
    
    `imgui.ini` において、ウィンドウの位置やサイズ、折りたたみ状態が変更されました。
    
    `main.cpp` において、コメントのフォーマットが修正され、`WinMain` 関数の引数が追加されました。
    
    `imgui.vcxproj.FileListAbsolute.txt` に変更が加えられましたが、具体的な変更内容は記載されていません。
    
    `CL.10232.write.1.tlog` において、ImGui 関連のソースファイルとオブジェクトファイルが追加されました。
    
    `imgui.lib.recipe` に新しい XML ファイルが追加されました。
    
    `CL.command.1.tlog` において、ImGui 関連のソースファイルのコンパイルコマンドが追加されました。
    
    `Cl.items.tlog` において、ImGui 関連のソースファイルとオブジェクトファイルが追加されました。
    
    `CL.write.1.tlog` ファイルに、複数のソースファイル（`IMGUI.CPP`、`IMGUI_DEMO.CPP`、`IMGUI_DRAW.CPP`、`IMGUI_IMPL_DX12.CPP`、`IMGUI_IMPL_WIN32.CPP`、`IMGUI_TABLES.CPP`、`IMGUI_WIDGETS.CPP`）と、それらに対応するオブジェクトファイル（`IMGUI_WIDGETS.OBJ`、`IMGUI_TABLES.OBJ`、`IMGUI_IMPL_WIN32.OBJ`、`IMGUI_IMPL_DX12.OBJ`、`IMGUI_DRAW.OBJ`、`IMGUI_DEMO.OBJ`、`IMGUI.OBJ`）およびデバッグ情報ファイル（`IMGUI.PDB`）が追加されました。
    
    `imgui.lastbuildstate` ファイルに、`PlatformToolSet`、`VCToolArchitecture`、`VCToolsVersion`、`TargetPlatformVersion` の情報が追加されました。
    
    `Lib-link.read.1.tlog` ファイルに、複数のオブジェクトファイル（`IMGUI.OBJ`、`IMGUI_DEMO.OBJ`、`IMGUI_DRAW.OBJ`、`IMGUI_IMPL_DX12.OBJ`、`IMGUI_IMPL_WIN32.OBJ`、`IMGUI_TABLES.OBJ`、`IMGUI_WIDGETS.OBJ`）およびシステムファイル（`TZRES.DLL`、`SORTDEFAULT.NLS`）が追加されました。
    
    `Lib-link.write.1.tlog` ファイルに、複数のオブジェクトファイル（`IMGUI.OBJ`、`IMGUI_DEMO.OBJ`、`IMGUI_DRAW.OBJ`、`IMGUI_IMPL_DX12.OBJ`、`IMGUI_IMPL_WIN32.OBJ`、`IMGUI_TABLES.OBJ`、`IMGUI_WIDGETS.OBJ`）およびライブラリファイル（`IMGUI.LIB`）が追加されました。
    
    `Lib.command.1.tlog` ファイルに、複数のオブジェクトファイル（`IMGUI.OBJ`、`IMGUI_DEMO.OBJ`、`IMGUI_DRAW.OBJ`、`IMGUI_IMPL_DX12.OBJ`、`IMGUI_IMPL_WIN32.OBJ`、`IMGUI_TABLES.OBJ`、`IMGUI_WIDGETS.OBJ`）およびライブラリファイルの出力コマンド（`/OUT:"IMGUI.LIB"`）が追加されました。

--- engine/base/imGui/ImguiSetup.h
+++ engine/base/imGui/ImguiSetup.h
@@ -13,9 +13,9 @@
 #include "DirectXCore.h"
 //---------------------------------------
 // ImGui
+#include "imgui.h"
 #include <imgui_impl_win32.h>
 #include <imgui_impl_dx12.h>
-#include "imgui.h"
 
 //---------------------------------------
 // imguiStyle
